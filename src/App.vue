65bf39a58fba3dc0a5399c6e7317e8a6c94847c0
Breno Henrique <brenosc2@hotmail.com>
2017-03-06 01:36:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <CcHello></CcHello>
+  </div>
+</template>
+
+<script>
+import CcHello from './components/Hello'
+
+export default {
+  name: 'BrenoHenrique',
+  components: {
+    CcHello
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Breno Henrique</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1{
+    font-weight: bold;
+    font-size: 50px;
+    color: red;
+  }
+</style>
