a4d4d03e044e2cce59e41444cd70076a40e990f1
Breno Henrique <brenosc2@hotmail.com>
2017-03-06 02:15:58 -0300

    adding left sidebar behavior

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="phone-viewport">
     <md-toolbar>
-      <h1 class="md-title">Mini Ecommerce VueJs</h1>
+      <md-button class="md-icon button" v-on:click.native="toggleSideNav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h1 class="md-title">{{ title }}</h1>
     </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSideNav">
+      <md-toolbar class="md-medium">
+        <div class="md-toolbar-container">
+          <h2 class="md-title">Sidenav Content</h2>
+        </div>
+      <md-toolbar>
+
+      <p>ADUHAsudhash aushd uashdu hasudh aushduashduahsudhasuhdaush duash udhasu dhasuhd uashdu ahsudhas udhas</p>
+
+    </md-sidenav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'WebschoolComponent'
+  name: 'WebschoolComponent',
+  data () {
+    return {
+      title: 'Ecommerce VueJS'
+    }
+  },
+  methods: {
+    toggleSideNav () {
+      this.$refs.leftSideNav.toggle()
+    }
+  }
 }
 </script>
 
