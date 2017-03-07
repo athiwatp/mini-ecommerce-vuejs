05da10751e27de7af8c99375911bdcddc5c9f953
Breno Henrique <brenosc2@hotmail.com>
2017-03-07 14:03:43 -0300

    adding /products and /categories using vue-router

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,35 @@
       <md-button class="md-icon button" v-on:click.native="toggleSideNav">
         <md-icon>menu</md-icon>
       </md-button>
-
       <h1 class="md-title">{{ title }}</h1>
     </md-toolbar>
 
     <md-sidenav class="md-left" ref="leftSideNav">
       <md-toolbar class="md-medium">
         <div class="md-toolbar-container">
-          <h2 class="md-title">Sidenav Content</h2>
+          <h2 class="md-title">Navegação</h2>
         </div>
       <md-toolbar>
 
-      <p>ADUHAsudhash aushd uashdu hasudh aushduashduahsudhasuhdaush duash udhasu dhasuhd uashdu ahsudhas udhas</p>
-
+      <md-list>
+        <md-list-item>
+          <router-link to="/person">Perfil</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/categories">Categorias</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/products">Produtos</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/logout">Sair</router-link>
+        </md-list-item>
+      </md-list>
     </md-sidenav>
+
+    <md-row>
+      <router-view></router-view>
+    </md-row>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
 import App from './App'
+import Categories from './components/categories.vue'
+import Products from './components/products.vue'
+import Person from './components/person.vue'
 
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
+
+const routes = [{
+  path: '/person',
+  component: Person
+}, {
+  path: '/categories',
+  Component: Categories
+}, {
+  path: '/products',
+  component: Products
+}]
+
+const router = new VueRouter({
+  routes
+})
+
 Vue.material.registerTheme('default', {
   primary: 'blue',
   accent: 'red',
@@ -13,6 +35,6 @@ Vue.material.registerTheme('default', {
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  router,
   render: h => h(App)
-})
+}).$mount('#app')
